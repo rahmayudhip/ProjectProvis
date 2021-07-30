7450505ba127efd47c88b43011a2d7b61ace809f
Rahmayudhi <72844863+rahmayudhip@users.noreply.github.com>
2021-07-30 14:12:15 +0700

    Form Mhs
    Make a function etc. for FormMahasiswa

---
+++ FormMahasiswa/src/formmahasiswa/JTatto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formmahasiswa;
+
+import java.awt.Component;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class JTatto extends Component {
+
+    public JTatto() {
+    }
+    
+}
